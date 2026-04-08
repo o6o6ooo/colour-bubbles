47d96c18d9a3a72c1a953495157f76acde8c970d
Sakura Wallace <066sakura@gmail.com>
2026-04-08 02:07:51 +0100

    ui: update favicon icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Colour Bubbles",
   description: "Get inspired by colour bubbles.",
+  icons: {
+    icon: [{ url: "/icon.svg", type: "image/svg+xml" }],
+  },
 };
 
 export default function RootLayout({
